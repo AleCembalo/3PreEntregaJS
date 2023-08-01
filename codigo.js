e045da9cb94f5e42659e2a8b2dae5e797db5f525
AleCembalo <alejandracembalo@hotmail.com>
2023-08-01 13:07:05 -0300

    Menu

--- codigo.js
+++ codigo.js
@@ -2,74 +2,129 @@
 let tarjetaCepas = document.getElementById('tarjeta');
 let tablaBody = document.getElementById('tablabody');
 let totalCompra = document.getElementById('total');
+let botonTerminar = document.getElementsByClassName('botonTerminar');
 
 
-function renderizarCepas(cepas){
-
-    tarjetaCepas.innerHTML='';
-
-    for (const cepa of cepas){
-    tarjetaCepas.innerHTML += `
-                <section class="col-sm-12 col-md-6 col-lg-4 col-xl-4 tarjeta">    
-                    <div class="imagen">
-                    <img class="coco" src="${cepa.foto}" alt="cepaFoto">
-                        <div class="icono">
-                            <strong>PACK X 3<br>$ ${cepa.precio}</strong>
-                        </div>
-                    </div>
-                    <div class="texto1">
-                        ${cepa.nombre}
-                    </div>
-                    <div class="texto2">
-                        ${cepa.familia}
-                    </div>
-                    <div class="texto3">
-                        <div class="izquierda">
-                            <div>
-                                THC ${cepa.thc}%
+function renderizarCepas(stock){
+    tarjetaCepas.innerHTML =``
+    for (const cepa of stock){
+        tarjetaCepas.innerHTML += `
+                    <section class="col-sm-12 col-md-6 col-lg-4 col-xl-4 tarjeta">    
+                        <div class="imagen">
+                        <img class="coco" src="${cepa.foto}" alt="cepaFoto">
+                            <div class="icono">
+                                <strong>PACK X 3<br>$ ${cepa.precio}</strong>
                             </div>
                         </div>
-                        <div class="centro">
-                            <div>
-                                CBD ${cepa.cbd}%
+                        <div class="texto1">
+                            ${cepa.nombre}
+                        </div>
+                        <div class="texto2">
+                            ${cepa.familia}
+                        </div>
+                        <div class="texto3">
+                            <div class="izquierda">
+                                <div>
+                                    THC ${cepa.thc}%
+                                </div>
+                            </div>
+                            <div class="centro">
+                                <div>
+                                    CBD ${cepa.cbd}%
+                                </div>
+                            </div>
+                            <div class="derecha">
+                                <div>
+                                    ${cepa.tipo}
+                                </div>
                             </div>
                         </div>
-                        <div class="derecha">
-                            <div>
-                                ${cepa.tipo}
-                            </div>
+                        <hr>
+                        <div class="texto4">
+                            <button id=${cepa.id} type="button" data-bs-toggle="modal" data-bs-target="#myModal" class="comprar">
+                                <i class="fa-solid fa-cart-arrow-down" style="color: #ffffff;"></i>
+                                <span>Agregar al carrito</span>
+                            </button>
                         </div>
-                    </div>
-                    <hr>
-                    <div class="texto4">
-                        <button id=${cepa.id} type="button" data-bs-toggle="modal" data-bs-target="#myModal" class="comprar">
-                            <i class="fa-solid fa-cart-arrow-down" style="color: #ffffff;"></i>
-                            <span>Agregar al carrito</span>
-                        </button>
-                    </div>
-                </section>
-    `;
-    }
-
-    let botonesDeCompra = document.getElementsByClassName('comprar');
-        for (const boton of botonesDeCompra){
-            boton.addEventListener('click',()=>{
-                const semaAlCarro = cepas.find((cepa) => cepa.id == boton.id);
-                agregarACarrito(semaAlCarro);
-            });
-        }
-
-    function agregarACarrito(cepa){
-        carrito.push(cepa);
-        tablaBody.innerHTML +=`
-            <tr>
-                <td>${cepa.nombre}</td>
-                <td>${cepa.precio}</td>
-            </tr>`;
-        const sumaTotal = carrito.reduce((acumulador,cepa)=>acumulador+cepa.precio,0);
-        totalCompra.innerHTML = sumaTotal;
+                    </section>`;
     }
 }
 
 renderizarCepas(cepas);
+
+// MENU
+
+let botonFeminizadas = document.getElementById('feminizadas');
+let botonRegulares = document.getElementById('regulares');
+let botonSativas = document.getElementById('sativas');
+let botonHibridas = document.getElementById('hibridas');
+let botonIndicas = document.getElementById('indicas');
+let botonInicio = document.getElementById('inicio');
+
+botonFeminizadas.addEventListener('click',()=>{
+    const feminizadas = cepas.filter (cepa=> cepa.sexo === 'Feminizada');
+    console.log(feminizadas);
+    renderizarCepas(feminizadas);
+    }
+)
+
+botonRegulares.addEventListener('click',()=>{
+    const regulares = cepas.filter (cepa=> cepa.sexo === 'Regular');
+    console.log(regulares);
+    renderizarCepas(regulares);
+    }
+)
+
+botonSativas.addEventListener('click',()=>{
+    const sativas = cepas.filter (cepa=> cepa.tipo === 'Sativa');
+    console.log(sativas);
+    renderizarCepas(sativas);
+    }
+)
+
+botonHibridas.addEventListener('click',()=>{
+    const hibridas = cepas.filter (cepa=> cepa.tipo === 'Hibrida');
+    console.log(hibridas);
+    renderizarCepas(hibridas);
+    }
+)
+
+botonIndicas.addEventListener('click',()=>{
+    const indicas = cepas.filter (cepa=> cepa.tipo === 'Indica');
+    console.log(indicas);
+    renderizarCepas(indicas);
+    }
+)
+
+botonInicio.addEventListener('click',()=>{
+    renderizarCepas(cepas);
+    }
+)
+
+let botonesDeCompra = document.getElementsByClassName('comprar');
+    for (const boton of botonesDeCompra){
+        boton.addEventListener('click',()=>{
+        const semaAlCarro = cepas.find((cepa) => cepa.id == boton.id);
+        agregarACarrito(semaAlCarro);
+        });
+    }
+
+function agregarACarrito(cepa){
+    carrito.push(cepa); 
+    tablaBody.innerHTML +=`
+        <tr>
+            <td>${cepa.nombre}</td>
+            <td>${cepa.precio}</td>
+        </tr>`;
+    const sumaTotal = carrito.reduce((acumulador,cepa)=>acumulador+cepa.precio,0);
+    totalCompra.innerHTML = sumaTotal;
+}
+
+function terminar(){
+    carrito.splice(0, carrito.length);
+    tablaBody.innerHTML =`
+        `;
+}
+
+
 
